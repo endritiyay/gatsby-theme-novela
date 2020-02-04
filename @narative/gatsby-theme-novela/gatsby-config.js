3a8ef99350434344c8636597c357b4c11dd3d7c4
endritiyay <endrithyseni1997@gmail.com>
2020-02-04 10:49:36 +0100

    Update gatsby-config.js

--- @narative/gatsby-theme-novela/gatsby-config.js
+++ @narative/gatsby-theme-novela/gatsby-config.js
@@ -1,248 +1,68 @@
-/* eslint-disable */
-
-module.exports = ({
-  contentAuthors = 'content/authors',
-  contentPosts = 'content/posts',
-  pathPrefix = '',
-  sources: { local, contentful } = { local: true, contentful: false },
-}) => ({
-  pathPrefix,
-  mapping: {
-    'Mdx.frontmatter.author': `AuthorsYaml`,
+equire('dotenv').config();
+module.exports = {
+  siteMetadata: {
+    title: `Dieta Bytyqi`,
+    name: `Dieta Bytyqi`,
+    siteUrl: `https://www.dietabytyqi.com`,
+    description: `Artikuj dhe shkrime te ndryshme`,
+    hero: {
+      heading: `Kur pranvera hesht dhe mëngjeset flenë, më duhet veç një melodi. Unë dhe ulësja e përkyer, duke udhëtuar drejt imagjinatës sime.`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/dietabytyqi`,
+      },
+      
+      {
+        name: `instagram`,
+        url: `https://instagram.com/dietabytyqi`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/dietabytyqi`,
+      },
+      {
+        name: `facebook`,
+        url: `https://www.facebook.com/dieta.bytyci`,
+      },
+    ],
   },
   plugins: [
-    `gatsby-plugin-typescript`,
-    `gatsby-image`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
-    `gatsby-transformer-yaml`,
-    `gatsby-plugin-theme-ui`,
     {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        setup: ({
-          query: {
-            site: { siteMetadata },
-          },
-          ...rest
-        }) => {
-          siteMetadata.feed_url = siteMetadata.siteUrl + '/rss.xml';
-          siteMetadata.image_url =
-            siteMetadata.siteUrl + '/icons/icon-512x512.png';
-          const siteMetadataModified = siteMetadata;
-          siteMetadataModified.feed_url = `${siteMetadata.siteUrl}/rss.xml`;
-          siteMetadataModified.image_url = `${siteMetadata.siteUrl}/icons/icon-512x512.png`;
+    resolve: 'gatsby-source-contentful',
+    options: {
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
+  },
 
-          return {
-            ...siteMetadataModified,
-            ...rest,
-          };
-        },
-        feeds: [
-          {
-            serialize: ({ query: { site, allArticle, allContentfulPost } }) => {
-              if (local && !contentful) {
-                return allArticle.edges
-                  .filter(edge => !edge.node.secret)
-                  .map(edge => {
-                    return {
-                      ...edge.node,
-                      description: edge.node.excerpt,
-                      date: edge.node.date,
-                      url: site.siteMetadata.siteUrl + edge.node.slug,
-                      guid: site.siteMetadata.siteUrl + edge.node.slug,
-                      // custom_elements: [{ "content:encoded": edge.node.body }],
-                      author: edge.node.author,
-                    };
-                  });
-              } else if (!local && contentful) {
-                return allContentfulPost.edges
-                  .filter(edge => !edge.node.secret)
-                  .map(edge => {
-                    return {
-                      ...edge.node,
-                      description: edge.node.excerpt,
-                      date: edge.node.date,
-                      url: site.siteMetadata.siteUrl + edge.node.slug,
-                      guid: site.siteMetadata.siteUrl + edge.node.slug,
-                      // custom_elements: [{ "content:encoded": edge.node.body }],
-                      author: edge.node.author,
-                    };
-                  });
-              } else {
-                const allArticlesData = { ...allArticle, ...allContentfulPost };
-                return allArticlesData.edges
-                  .filter(edge => !edge.node.secret)
-                  .map(edge => {
-                    return {
-                      ...edge.node,
-                      description: edge.node.excerpt,
-                      date: edge.node.date,
-                      url: site.siteMetadata.siteUrl + edge.node.slug,
-                      guid: site.siteMetadata.siteUrl + edge.node.slug,
-                      // custom_elements: [{ "content:encoded": edge.node.body }],
-                      author: edge.node.author,
-                    };
-                  });
-              }
-            },
-            query:
-              local && !contentful
-                ? `
-              {
-                allArticle(sort: {order: DESC, fields: date}) {
-                  edges {
-                    node {
-                      excerpt
-                      date
-                      slug
-                      title
-                      author
-                      secret
-                    }
-                  }
-                }
-              }
-              `
-                : !local && contentful
-                ? `
-              {
-                allContentfulPost(sort: {order: DESC, fields: date}) {
-                  edges {
-                    node {
-                      excerpt
-                      date
-                      slug
-                      title
-                      author {
-                        name
-                      }
-                      secret
-                    }
-                  }
-                }
-              }
-              `
-                : `
-              {
-                allArticle(sort: {order: DESC, fields: date}) {
-                  edges {
-                    node {
-                      excerpt
-                      date
-                      slug
-                      title
-                      author
-                      secret
-                    }
-                  }
-                }
-                allContentfulPost(sort: {order: DESC, fields: date}) {
-                  edges {
-                    node {
-                      excerpt
-                      date
-                      slug
-                      title
-                      author {
-                        name
-                      }
-                      secret
-                    }
-                  }
-                }
-              }
-              `,
-            output: '/rss.xml',
-          },
-        ],
+     {
+    resolve: '@narative/gatsby-theme-novela',
+    options: {
+      contentPosts: 'content/posts',
+      contentAuthors: 'content/authors',
+      rootPath: '/',
+      basePath: '/',
+      mailchimp: false,
+      sources: {
+        local: false,
+        contentful: true,
       },
     },
+  },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        path: contentPosts,
-        name: contentPosts,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: contentAuthors,
-        name: contentAuthors,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 10000,
-              linkImagesToOriginal: false,
-              quality: 80,
-              withWebp: true,
-            },
-          },
-          {
-            resolve: `@raae/gatsby-remark-oembed`,
-            options: {
-              providers: {
-                include: ["Instagram"]
-              }
-            }
-          },
-          {
-            resolve: "gatsby-remark-embed-video",
-            options: {
-              width: 680,
-              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              height: 400, // Optional: Overrides optional.ratio
-              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
-              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
-              urlOverrides: [
-                {
-                  id: 'youtube',
-                  embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
-                }
-              ] //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
-            }
-          },
-          { resolve: `gatsby-remark-copy-linked-files` },
-          { resolve: `gatsby-remark-numbered-footnotes` },
-          { resolve: `gatsby-remark-smartypants` },
-          {
-            resolve: 'gatsby-remark-external-links',
-            options: {
-              target: '_blank',
-              rel: 'noreferrer', // eslint-disable-line unicorn/prevent-abbreviations
-            },
-          },
-        ],
-        remarkPlugins: [require(`remark-slug`)], // eslint-disable-line global-require
-      },
-    },
-    {
-      resolve: `gatsby-plugin-emotion`,
-      options: {
-        displayName: process.env.NODE_ENV === `development`,
+        name: `Dieta Bytyqi`,
+        short_name: `Dieta`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/components/images/eta.png`,
       },
     },
   ],
-});
+};
